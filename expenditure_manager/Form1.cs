ebdb00f565f8f752d2877c0887f613d7cf8a79dd
rajaomariajaona <rajaomariajaona11@gmail.com>
2019-10-12 09:12:50 +0300

    cheat

---
+++ expenditure_manager/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expenditure_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void bunifuThinButton26_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bunifuImageButton3_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            
+        }
+    }
+}
